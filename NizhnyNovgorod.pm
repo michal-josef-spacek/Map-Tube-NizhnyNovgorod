0fe60b67c96f630cfa63d4b7816eac908291b9e4
Michal Josef Špaček <tupinek@gmail.com>
2025-01-22 11:22:21 +0100

    Remove simple comments

--- NizhnyNovgorod.pm
+++ NizhnyNovgorod.pm
@@ -1,11 +1,9 @@
 package Map::Tube::NizhnyNovgorod;
 
-# Pragmas.
 use strict;
 use warnings;
 use 5.006;
 
-# Modules.
 use File::Share ':all';
 use Moo;
 use namespace::clean;
@@ -117,11 +115,9 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
 
 =head1 EXAMPLE1
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(decode_utf8 encode_utf8);
  use Map::Tube::NizhnyNovgorod;
 
@@ -139,11 +135,9 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
 
 =head1 EXAMPLE2
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::NizhnyNovgorod;
 
  # Object.
@@ -160,11 +154,9 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
 
 =head1 EXAMPLE3
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Map::Tube::GraphViz;
  use Map::Tube::GraphViz::Utils qw(node_color_without_label);
  use Map::Tube::NizhnyNovgorod;
@@ -198,11 +190,9 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
 
 =head1 EXAMPLE4
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(encode_utf8);
  use Map::Tube::NizhnyNovgorod;
 
@@ -221,11 +211,9 @@ For more information about NizhnyNovgorod Map, click L<here|https://en.wikipedia
 
 =head1 EXAMPLE5
 
- # Pragmas.
  use strict;
  use warnings;
 
- # Modules.
  use Encode qw(decode_utf8 encode_utf8);
  use Map::Tube::NizhnyNovgorod;
 
